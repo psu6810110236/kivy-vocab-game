9d1af571b4162cda1fdd10b1086dcb9a6f18f651
Warit Phetkaeo <6810110318@psu.ac.th>
2026-02-26 23:51:07 +0700

    connect HP system with wrong answer logic

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from systems.sound_manager import SoundManager
+from systems.hp_system import HPSystem
+
+class MainLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(orientation="vertical", **kwargs)
+
+        # สร้างตัวจัดการเสียง
+        self.sound = SoundManager()
+        self.hp = HPSystem(max_hp=3)
+
+        # ปุ่ม Correct
+        btn_correct = Button(text="Correct", font_size=32)
+        btn_correct.bind(on_press=lambda x: self.sound.play_correct())
+
+        # ปุ่ม Wrong
+        btn_wrong = Button(text="Wrong", font_size=32)
+        btn_wrong.bind(on_press=self.on_wrong)
+
+
+        # ปุ่ม Game Over
+        btn_gameover = Button(text="Game Over", font_size=32)
+        btn_gameover.bind(on_press=lambda x: self.sound.play_gameover())
+
+        self.add_widget(btn_correct)
+        self.add_widget(btn_wrong)
+        self.add_widget(btn_gameover)
+    def on_wrong(self, instance):
+        self.hp.take_damage()
+        self.hp_label.text = f"HP: {self.hp.current_hp}"
+        self.sound.play_wrong()
+
+        if self.hp.is_dead():
+            print("GAME OVER")
 
 class VocabGameApp(App):
     def build(self):
-        return Label(text='Welcome to English Vocab Game!')
+        return MainLayout()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     VocabGameApp().run()
